d871a5c3bf77493ef67316f692267a76ffe7dc99
Precious Udoibok <preciousudoibok@gmail.com>
2024-08-09 12:41:53 +0100

    CORS updated

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import phone,user,authentication
+from starlette.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # List allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # List allowed methods or use ["*"] for all methods
+    allow_headers=["*"],  # List allowed headers or use ["*"] for all headers
+)
 
 #register your route in the main.py
 app.include_router(phone.router)
